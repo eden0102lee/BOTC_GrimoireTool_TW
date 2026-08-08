0675f559cc794b71304d9392f33207021296b5fc
eden0102lee <eden0102lee@gmail.com>
2026-08-09 03:02:20 +0800

    Load stacking system after visual layers

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import "./game-ui.scss";
 import "./play-ui.scss";
 import "./layout-polish.scss";
 import "./ux-polish.scss";
+import "./stacking-system.scss";
 import store from "./store";
 import i18n from "./i18n";
 import { library } from "@fortawesome/fontawesome-svg-core";
