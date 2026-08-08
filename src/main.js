4686f36bcc3df972a6aa7e33484fbe548ab7ea65
eden0102lee <eden0102lee@gmail.com>
2026-08-09 01:28:23 +0800

    Load grimoire art direction styles

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App";
+import "./art-direction.scss";
 import store from "./store";
 import i18n from "./i18n";
 import { library } from "@fortawesome/fontawesome-svg-core";
